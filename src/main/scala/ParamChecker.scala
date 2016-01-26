5b007ed82c2c96afeb8c29173eb25ddc08e41456
Volts <svoltsss@gmail.com>
2016-01-27 01:44:46 +0900

    check

--- src/main/scala/ParamChecker.scala
+++ src/main/scala/ParamChecker.scala
@@ -21,43 +21,63 @@ case class LT_ab(leftName:String,rightName:String) extends CheckRule
 
 object ParamChecker extends ExcelWorkBook{
   def main(args: Array[String]):Unit ={
-    check(
-      "yourfilepath",
-      "sheetname",
-      GT_ab("入力","")
+    val c = check(
+      args(0),
+      args(1),
+      GT_ab("check left","check right")
     )
+    println("CHECK1: " +c)
 
   }
 
-  def check(checkFile:String,checkSheetName:String,checkRule:CheckRule): Unit ={
+  def check(checkFile:String,checkSheetName:String,checkRule:CheckRule): Boolean ={
     val e = new ExcelWorkBook()
     e.bookRead(checkFile)
     e.selectSheet(checkSheetName)
     checkRule match {
-      case GT_ab(left,right) => getCheckRange(e.getSheet,left,15) /*greaterThan(left,right)*/
+      case GT_ab(left,right) => greaterThan(e.getSheet,left,right)
     }
 
   }
 
-  def greaterThan(leftName:String,rightName:String): Boolean = {
-    false
+  def greaterThan(sheet:Sheet, leftName:String,rightName:String): Boolean = {
+    val leftCheckCol = getCheckCol(sheet, leftName, 5, 1)
+    val rightCheckCol = getCheckCol(sheet, rightName, 5, 1)
+    leftCheckCol match {
+      case Some(ls) => rightCheckCol match {
+        case Some(rs) => (ls zip rs).filter(t => t._1.getNumericCellValue > t._2.getNumericCellValue).size <= 0
+        case None => false
+      }
+      case None => false
+    }
   }
 
-  def getCheckRange(sheet: Sheet,name:String,startRow:Int):Option[Seq[Cell]] = {
-    val rs = Range(sheet.getFirstRowNum , startRow).map(n => sheet.getRow(n))
+  def getCheckCol(sheet: Sheet,name:String,searchRow:Int,dataOffset:Int):Option[Seq[Cell]] = {
+    searchIndexCell(name,sheet,searchRow) match {
+      case Some(c) => Some(getColumn(sheet, c.getColumnIndex, c.getRowIndex + dataOffset, sheet.getLastRowNum))
+      case None => None
+    }
+  }
+
+  def getColumn(sheet:Sheet, colNo:Int, startRow:Int, lastRow:Int):Seq[Cell] = {
+    val cols : ArrayBuffer[Cell] = new ArrayBuffer[Cell]
+    for(i:Int <- startRow to lastRow){
+      cols += sheet.getRow(i).getCell(colNo)
+    }
+    cols.toSeq
+  }
+
+  /**
+   * return Cell (NonNull,String)
+   */
+  def searchIndexCell(indexName:String,sheet:Sheet,searchLine:Int):Option[Cell] = {
+    val firstNum = sheet.getFirstRowNum
+    val searchNum = firstNum + searchLine
+    val rs = Range(firstNum , searchNum).map(n => sheet.getRow(n))
     val lr:List[List[Cell]] =  rs.map(r => row2List(r)).toIndexedSeq.toList
-    println("input:" + name)
-    val strCell:List[Cell] = lr.map(r => r.filter(c => c!=null).filter(c => c.getCellType==1)).flatten
-    val cc = strCell.filter(c => c.getStringCellValue.contains(name))
-
-    println("contains cell is :" +cc)
-
-    val hc = strCell.filter(c => c.getStringCellValue == name)
-    println("hit cell is :" + hc)
-    println("hit cell Row:" + hc(0).getRowIndex + "  Col:" + hc(0).getColumnIndex)
-
-    if (hc.length >= 1) Some(hc.flatten) else None
-
+    val sc:List[Cell] = lr.map(r => r.filter(c => c!=null).filter(c => c.getCellType==1)).flatten
+    val hc = sc.filter(c => c.getStringCellValue == indexName)
+    if(hc.size != 0) Some(hc.head) else None
   }
 
   def print2Unicode(s : String): Unit ={
